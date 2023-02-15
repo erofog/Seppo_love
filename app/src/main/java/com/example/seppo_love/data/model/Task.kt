342388f7d6ae96717afeeb298e5746ab5395b10a
seppo <sorsan_seppo@proton.me>
2023-02-15 15:39:30 +0200

    something for the model

---
+++ app/src/main/java/com/example/seppo_love/data/model/Task.kt
@@ -0,0 +1,8 @@
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val description: String,
+    val dueDate: Date,
+    val priority: Priority
+)
